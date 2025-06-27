2cd3857fc32433370f850c692fb6ddd4716bc947
iliasFs <ilias.fasilakhs@gmail.com>
2025-06-27 22:43:51 +0300

    fix

--- api/import.js
+++ api/import.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://auto-drop-x.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin", "https://auto-drop-x.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
